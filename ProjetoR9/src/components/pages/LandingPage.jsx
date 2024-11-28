93f0b110c56e6921e20a6cc755015c5c62da2568
F. Ricardo <112594292+FelipeRicardo5@users.noreply.github.com>
2024-11-28 21:27:47 +0000

    update to landingpage

--- ProjetoR9/src/components/pages/LandingPage.jsx
+++ ProjetoR9/src/components/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './LandingPage.module.css';
+import SubmitButton from '../../components/form/SubmitButton'
 
 import LogoReacao from '../../assets/reacao.png'
 import User from '../../assets/password.png'
@@ -10,18 +11,36 @@ function LandingPage() {
         <div>
             <nav className={styles.navbar}>
                 <img src={LogoReacao} alt='LogoReacao'/>
-                <a className={styles.navbar_p} >CONHEÇA A<br/>
+                <a href='#' className={styles.navbar_p} >CONHEÇA A<br/>
                 RE9AÇÃO</a>
-                <a className={styles.navbar_p} >PERGUNTAS<br/>
+                <a href='#' className={styles.navbar_p} >PERGUNTAS<br/>
                 FREQUENTES</a>
-                <a className={styles.navbar_p} >BLOG DA<br/>
+                <a href='#' className={styles.navbar_p} >BLOG DA<br/>
                 RE9AÇÃO</a>
                 <div style={{display:'flex', alignItems: 'center', textAlign: 'left'}} >
                     <img style={{width: '50px'}} src={User} alt='user'/>
                     <p style={{color: 'rgb(0, 0, 0)'}} >SEJA BEM VINDO!<br/>
-                    <a style={{color: '#EF6B01'}} >Entre</a> ou <a style={{color: '#EF6B01'}} >Cadastre-se</a></p>
+                    <a href='#' style={{color: '#EF6B01'}} >Entre</a> ou <a href='#' style={{color: '#EF6B01'}} >Cadastre-se</a></p>
                 </div>
             </nav>
+            <main style={{display: 'flex'}} >
+
+                <div className={styles.campText} >
+                    <h1>
+                    Conectando<br/> talentos a<br/> desafios<br/> reais do<br/> mercado
+                    </h1>
+                    <h3>
+                    Nossa plataforma une alunos de graduação a empresas, criando projetos inovadores e experiências de trabalho no mundo real.
+                    </h3>
+                    <SubmitButton 
+                    text={'Saiba mais'}
+                    />
+                </div>
+                <div className={styles.campImg} >
+                    <img src={User} />
+                </div>
+
+            </main>
 
         </div>
     );
